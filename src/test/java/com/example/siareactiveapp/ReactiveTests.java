c2f3f6d0f756367bcacef22392f8e69a8881dd11
VasyanPupkin <tgoroshek@gmail.com>
2025-04-28 21:10:34 +0300

    335 стр законч

---
+++ src/test/java/com/example/siareactiveapp/ReactiveTests.java
@@ -0,0 +1,186 @@
+package com.example.siareactiveapp;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ReactiveTests {
+
+    @Test
+    public void createAFlux_just() {
+        Flux<String> fruitFlux = Flux //Создали продьюсер, содержащий прикладные объекты
+                .just("Apple", "Orange", "Grape", "Banana", "Strawberry");
+
+        //Подписались на продьюсер с помощью сабскрайбера (лямбдой задано действие, которое консьюмер делает с полученными данными)
+        fruitFlux.subscribe(
+                f -> System.out.println("Here’s some fruit: " + f)
+        );
+
+        //Проверяет, прошли ли через реактивный стрим ожидаемые данные
+        StepVerifier.create(fruitFlux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
+
+    @Test
+    public void createAFlux_fromArray() {
+        //Реактивные стримы можно так же делать мз массивов, итераблов и обычных стримов
+        String[] fruits = new String[]{
+                "Apple", "Orange", "Grape", "Banana", "Strawberry"};
+        Flux<String> fruitFlux = Flux.fromArray(fruits);
+        StepVerifier.create(fruitFlux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
+
+    @Test
+    public void createAFlux_fromIterable() {
+
+        List<String> fruitList = new ArrayList<>();
+        fruitList.add("Apple");
+        fruitList.add("Orange");
+        fruitList.add("Grape");
+        fruitList.add("Banana");
+        fruitList.add("Strawberry");
+
+        Flux<String> fruitFlux = Flux.fromIterable(fruitList);
+
+        StepVerifier.create(fruitFlux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
+
+    @Test
+    public void createAFlux_fromStream() {
+        Stream<String> fruitStream =
+                Stream.of("Apple", "Orange", "Grape", "Banana", "Strawberry");
+
+        Flux<String> fruitFlux = Flux.fromStream(fruitStream);
+
+        StepVerifier.create(fruitFlux)
+                .expectNext("Apple")
+                .expectNext("Orange")
+                .expectNext("Grape")
+                .expectNext("Banana")
+                .expectNext("Strawberry")
+                .verifyComplete();
+    }
+
+    @Test
+    public void createAFlux_range() {
+        //Создает реактивный стрим с элементами в указанном диапазоне
+        Flux<Integer> intervalFlux =
+                Flux.range(1, 5);
+
+        StepVerifier.create(intervalFlux)
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .expectNext(5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void createAFlux_interval() {
+        //Интервал создает бесконечный данных поток с указанной задержкой между элементами
+        Flux<Long> intervalFlux =
+                Flux.interval(Duration.ofSeconds(1))
+                        .take(5); //Позволяет ограничить поток на выбранном количестве элементов
+
+        StepVerifier.create(intervalFlux)
+                .expectNext(0L)
+                .expectNext(1L)
+                .expectNext(2L)
+                .expectNext(3L)
+                .expectNext(4L)
+                .verifyComplete();
+    }
+
+    @Test
+    public void mergeFluxes() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa")
+                .delayElements(Duration.ofMillis(500)); //Задержка между элементами, что бы избежать каши в тесте
+
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples")
+                .delaySubscription(Duration.ofMillis(250)) //Задержка на подписку, что бы этот стрим пошел вторым
+                .delayElements(Duration.ofMillis(500));
+        //Позволяет слить два стрима в 1
+        Flux<String> mergedFlux = characterFlux.mergeWith(foodFlux);
+
+        StepVerifier.create(mergedFlux)//Потоки выше запускаются по цепочке вверх, когда происходит подписка на нижний поток
+                .expectNext("Garfield")
+                .expectNext("Lasagna")
+                .expectNext("Kojak")
+                .expectNext("Lollipops")
+                .expectNext("Barbossa")
+                .expectNext("Apples")
+                .verifyComplete();
+    }
+
+    @Test
+    public void zipFluxes() {
+
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa");
+
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+
+        //Позволяет "сшивать" 2 потока, сохраняя последовательность, для чего в прошлом тесте были применены таймауты
+        Flux<Tuple2<String, String>> zippedFlux =
+                Flux.zip(characterFlux, foodFlux);
+
+        StepVerifier.create(zippedFlux)
+                .expectNextMatches(p ->
+                        p.getT1().equals("Garfield") &&
+                                p.getT2().equals("Lasagna"))
+                .expectNextMatches(p ->
+                        p.getT1().equals("Kojak") &&
+                                p.getT2().equals("Lollipops"))
+                .expectNextMatches(p ->
+                        p.getT1().equals("Barbossa") &&
+                                p.getT2().equals("Apples"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void zipFluxesToObject() {
+        Flux<String> characterFlux = Flux
+                .just("Garfield", "Kojak", "Barbossa");
+
+        Flux<String> foodFlux = Flux
+                .just("Lasagna", "Lollipops", "Apples");
+
+        //Альтернативный способ зипования с указанием, что именно делать с объектами из стримов и как их объединять
+        Flux<String> zippedFlux =
+                Flux.zip(characterFlux, foodFlux, (c, f) -> c + " eats " + f);
+
+        StepVerifier.create(zippedFlux)
+                .expectNext("Garfield eats Lasagna")
+                .expectNext("Kojak eats Lollipops")
+                .expectNext("Barbossa eats Apples")
+                .verifyComplete();
+    }
+}
+
